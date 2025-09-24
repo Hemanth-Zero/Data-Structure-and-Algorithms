93b1388eddbc46c9cc9d3dc93cd0d6810027dc06
Hemanth-Zero <hemanthsagar0111@gmail.com>
2025-09-24 17:51:20 +0000

    Algorithm added

---
+++ Algorithms/MultiLargeNumbers.java
@@ -0,0 +1,20 @@
+public class MultiLargeNumbers {
+    public void algorithm(){
+            /*      
+                stoi(Sting A) return Integer A ;
+                MultiLargeNumbers(String A,String B){
+                    if( |A| < 5 && |b| < 5) 
+                        return  (stoi(A)*stoi(b)).toString()
+                    n = max(|A|,|B|)
+                    m = m/2
+                    a,b = split(A,m)
+                    c,d = split(B,m)
+                    P0 = MultiLargeNumbers(a,c)
+                    P2 = MultiLargeNumbers(b,d)
+                    P1 = MultiLargeNumbers(stradd(a,b),stradd(c,d))
+
+                    return Shift(P0,2m)+Shift(P1-P0-P2,m)+P2
+                }
+            */
+    }
+}
---
+++ Algorithms/linearTimeSelection.java
@@ -0,0 +1,26 @@
+
+public class linearTimeSelection {
+    public void algorithm(){
+        /*
+            The algorithm for linear time selection is as follows 
+            linearTimeSelection(A,k){
+                0. if(|A| < 5) return sort(A,k)
+                1. while i < n :
+                2.      chunks[j] = chunks[i to i+5]
+                3.      medi[j] = medians(chunks[j])
+                4.      i = i+5; j++;
+                5 median = medians(medi[0 to n/5])
+                6. L = {A[i] : A[i] < median }
+                7. R = {A[i] : A[i] > median }
+                8.if k >|L| linearTimeSelection(R,k-|L|)
+                9.else linearTimeSelection(L,k)
+            }
+        */
+    }
+    public void linearTimeSelection(){
+        
+    }
+    public static void main(String[] args){
+
+    }
+}
