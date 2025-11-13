fe53709babd73c0fe281c359e61e2f88298d6bf0
Hemanth-Zero <hemanthsagar0111@gmail.com>
2025-11-13 07:14:18 +0000

    function added

--- Algorithms/fractionalKnapsack.java
+++ Algorithms/fractionalKnapsack.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class fractionalKnapsack {
     public void algorithm(){
         /*
@@ -24,5 +25,36 @@ public class fractionalKnapsack {
 
             Total Time complexity = O(n log n)
         */
+    }
+    public static void knapsnak(int weight[],int values[],int C){
+        int n = weight.length;
+        int value = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]) ); // MAX HEAP TAKE
+        int ratio[] = new int[n];
+        for(int i = 0 ; i < n ; i ++){
+            ratio[i] = values[i]/weight[i];
+            pq.add(new int[]{ratio[i],i});
+        }
+        while(C > 0 || !pq.isEmpty()){
+            int[] item = pq.poll();
+            int i = item[1];
+            if(weight[i]<=C){
+                System.out.println(values[i] + " "+ i);
+                value += values[i];
+                C -= weight[i];
+            }
+            else{
+                value += (ratio[i]*C) ;
+                System.out.println(ratio[i]*C + "  "+ i);
+                C = 0;
+            }
+        }
+        System.out.println(" "+value);
+        return;
+    }
+    public static void main(String[] args){
+        int[] weights = {3,4,2,1};
+        int[] values = {1,2,3,4};
+        knapsnak(weights,values,4);
     }
 }
